38bbac082c6a3ccc0285d2147bbd16ec0e32214e
Dilbwag Singh <dilbwagsinghb.met18@itbhu.ac.in>
2019-07-10 19:59:25 +0530

    :octocat:Censored Keys

--- scapper.py
+++ scapper.py
@@ -1,11 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-consumer_key = 'vlQcinxCcBKCK4VD3gttj22iQ'
-consumer_secret = 'SIOWCT55CV4UmUXHDZEHJs0NsEQQq0Mj6wVQGbSpEVrsnFokdb'
+consumer_key = ''
+consumer_secret = ''
 
-access_token = '1101309721-1a8niUIgHDLQg2wwtkoMd0t9CwPKLneERfx8YDV'
-access_token_secret = 'Og4lL203iVxre73uQbTjRKbA8Lnheiiqy8K7RLgiEK9wU'
+access_token = ''
+access_token_secret = ''
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
